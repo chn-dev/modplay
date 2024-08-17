da8632dc176b52cd39fd4a3d445381b9856d596b
Christian Nowak <chnowak@web.de>
2024-08-17 14:47:44 +0200

    GPLv3

--- src/modplay/mixer.h
+++ src/modplay/mixer.h
@@ -1,7 +1,21 @@
-/*
- * Copyright (c) 2002, 2003, 2004, 2005, 2007 by Christian Nowak <chnowak@web.de>
- * Last update: 20th October, 2007
- */
+/*******************************************************************************
+ *  Copyright (c) 2002-2024 Christian Nowak <chnowak@web.de>                   *
+ *   This file is part of chn's modplay.                                       *
+ *                                                                             *
+ *  modplay is free software: you can redistribute it and/or modify it         *
+ *  under the terms of the GNU General Public License as published by the Free *
+ *  Software Foundation, either version 3 of the License, or (at your option)  *
+ *  any later version.                                                         *
+ *                                                                             *          
+ *  modplay is distributed in the hope that it will be useful, but             * 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License    *
+ *  for more details.                                                          *
+ *                                                                             *
+ *  You should have received a copy of the GNU General Public License along    *
+ *  with modplay. If not, see <https://www.gnu.org/licenses/>.                 *
+ *******************************************************************************/
+
 
 #ifndef __MIXER_H__
 #define __MIXER_H__
